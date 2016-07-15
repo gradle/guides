4c5356031fa17739966849abce2f0a7276e3cae4
Chris Beams <chris@gradle.com>
2016-07-15 15:48:17 +0200

    Centralize and simplify nginx configuration

--- _plugins/copy-guides-hook.rb
+++
@@ -1,4 +0,0 @@
-Jekyll::Hooks.register :site, :post_write do |site|
-  puts 'Copying guide content'
-  `_plugins/copy-guides.sh`
-end
--- _plugins/copy-guides.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-GUIDES_DIR=_guides
-
-for guide in $(find $GUIDES_DIR -type d -mindepth 1 -maxdepth 1 | xargs basename); do
-    cp -rv $GUIDES_DIR/$guide/build/asciidoc/html5/ _site/$guide
-done
