b9aeb11d8272864744f5c29f4fa5c2f61aed8b7b
Chris Beams <chris@gradle.com>
2016-09-09 14:29:19 +0200

    Add an option to stop nginx without also starting it

--- nginx.sh
+++ nginx.sh
@@ -4,5 +4,9 @@ if [ -f nginx.pid ]; then
     echo nginx pid $pid stopped.
 fi
 
+if [ "$1" == "stop" ]; then
+    exit 0;
+fi
+
 nginx -p . -c nginx.conf || exit 255
 echo nginx pid `cat nginx.pid` started. see http://localhost:8080
