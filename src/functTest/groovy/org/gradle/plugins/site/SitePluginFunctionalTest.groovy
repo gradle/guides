a83953059f7ba8fb2062df65da3f8a5b48db59eb
Benjamin Muschko <benjamin.muschko@gmail.com>
2017-04-02 11:48:44 -0400

    Use of extension in test case

--- src/functTest/groovy/org/gradle/plugins/site/SitePluginFunctionalTest.groovy
+++ src/functTest/groovy/org/gradle/plugins/site/SitePluginFunctionalTest.groovy
@@ -2,12 +2,30 @@ package org.gradle.plugins.site
 
 class SitePluginFunctionalTest extends AbstractFunctionalTest {
 
-    def "can generate site"() {
+    def "can generate site for default conventions"() {
         when:
         build('site')
 
         then:
         def indexFile = new File(projectDir,'build/docs/site/index.html')
+        indexFile.isFile()
+        indexFile.text == projectDir.name
+    }
+
+    def "can generate site for custom conventions"() {
+        given:
+        def customOutputDir = 'build/custom/site'
+        buildFile << """
+            site {
+                outputDir = file('$customOutputDir')
+            }
+        """
+
+        when:
+        build('site')
+
+        then:
+        def indexFile = new File(projectDir, "$customOutputDir/index.html")
         indexFile.isFile()
         indexFile.text == projectDir.name
     }
