3fad9f7fb716428bfe21de103e61157e90e80969
Benjamin Muschko <benjamin.muschko@gmail.com>
2017-04-19 10:42:02 -0400

    Assert created HTML

--- src/functTest/groovy/org/gradle/plugins/site/SitePluginFunctionalTest.groovy
+++ src/functTest/groovy/org/gradle/plugins/site/SitePluginFunctionalTest.groovy
@@ -1,5 +1,10 @@
 package org.gradle.plugins.site
 
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
+
 import static org.gradle.plugins.site.SitePlugin.SITE_TASK_NAME
 
 class SitePluginFunctionalTest extends AbstractFunctionalTest {
@@ -9,17 +14,23 @@ class SitePluginFunctionalTest extends AbstractFunctionalTest {
         build(SITE_TASK_NAME)
 
         then:
-        assertSiteFiles(new File(projectDir, 'build/docs/site'))
+        def outputDir = new File(projectDir, 'build/docs/site')
+        assertSiteFiles(outputDir)
+        def doc = parseIndexHtml(outputDir)
+        findWebsiteLinkDivs(doc).empty
+        findCodeLinkDivs(doc).empty
     }
 
     def "can generate site for custom conventions"() {
         given:
         def customOutputDir = 'build/custom/site'
+        def websiteUrl = 'https://gradle.org'
+        def vcsUrl = 'https://github.com/gradle/gradle'
         buildFile << """
             site {
                 outputDir = file('$customOutputDir')
-                websiteUrl = 'https://gradle.org'
-                vcsUrl = 'https://github.com/gradle/gradle'
+                websiteUrl = '$websiteUrl'
+                vcsUrl = '$vcsUrl'
             }
         """
 
@@ -27,14 +38,40 @@ class SitePluginFunctionalTest extends AbstractFunctionalTest {
         build(SITE_TASK_NAME)
 
         then:
-        assertSiteFiles(new File(projectDir, customOutputDir))
+        File outputDir = new File(projectDir, customOutputDir)
+        assertSiteFiles(outputDir)
+        def doc = parseIndexHtml(outputDir)
+        def foundWebSiteDivs = findWebsiteLinkDivs(doc)
+        foundWebSiteDivs.size() == 1
+        def webSiteLinks = findAhrefs(foundWebSiteDivs.first(), websiteUrl)
+        webSiteLinks.size() == 1
+        def foundVcsDivs = findCodeLinkDivs(doc)
+        foundVcsDivs.size() == 1
+        def vcsUrlLinks = findAhrefs(foundVcsDivs.first(), vcsUrl)
+        vcsUrlLinks.size() == 1
     }
 
     static void assertSiteFiles(File directory) {
-        assert new File(directory, "index.html").isFile()
-        assert new File(directory, "css/bootstrap.css").isFile()
-        assert new File(directory, "css/bootstrap-responsive.css").isFile()
-        assert new File(directory, "js/bootstrap.js").isFile()
-        assert new File(directory, "img/elephant-corner.png").isFile()
+        assert new File(directory, 'index.html').isFile()
+        assert new File(directory, 'css/bootstrap.css').isFile()
+        assert new File(directory, 'css/bootstrap-responsive.css').isFile()
+        assert new File(directory, 'js/bootstrap.js').isFile()
+        assert new File(directory, 'img/elephant-corner.png').isFile()
+    }
+
+    static Document parseIndexHtml(File outputDir) {
+        Jsoup.parse(new File(outputDir, 'index.html'), 'UTF-8')
+    }
+
+    static Elements findWebsiteLinkDivs(Document doc) {
+        doc.select('div[id=website-link]')
+    }
+
+    static Elements findCodeLinkDivs(Document doc) {
+        doc.select('div[id=code-link]')
+    }
+
+    static Elements findAhrefs(Element element, String url) {
+        element.select("a[href=$url")
     }
 }
