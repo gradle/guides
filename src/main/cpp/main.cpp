506cdcc46282b3963aafebdcb85eb29c33bf2517
Schalk W. Cronjé <schalk@gradle.com>
2017-05-04 21:22:55 +0200

    Remove `char** env` from function signature (gradle/guides#99)
    As per feedback from @lacasseio.

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>  // <1>
 #include "greeting.hpp" // <2>
 
-int main(int argc, char** argv, char** env) {
+int main(int argc, char** argv) {
     std::cout << greeting << std::endl;
     return 0;
 }
