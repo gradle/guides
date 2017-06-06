b75feb7ebbf0988edbaad79acafaf81314fbc944
Pepper Lebeck-Jobe <pepper@gradle.com>
2017-06-06 18:52:48 +0900

    Give the StoryTeller something to say when it has no Tales
    It will be more exciting for the example project.

--- src/example/fairy/src/main/java/org/gradle/fairy/app/StoryTeller.java
+++ src/example/fairy/src/main/java/org/gradle/fairy/app/StoryTeller.java
@@ -8,6 +8,9 @@ class StoryTeller {
 
     public static void main(String[] args) {
         ServiceLoader<Tale> loader = ServiceLoader.load(Tale.class);
+        if (!loader.iterator().hasNext()) {
+            System.out.println("Alas, I have no tales to tell!");
+        }
         for (Tale tale : loader) {
             tale.tell();
         }
