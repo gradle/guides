1bffd09f19a5ed5d62a367c722a8047e0faf4000
Pepper Lebeck-Jobe <pepper@gradle.com>
2017-06-09 15:29:32 +0900

    Update the final project with the results that work

--- src/final/formula/src/test/java/org/gradle/fairy/tale/formula/ModularityTest.java
+++ src/final/formula/src/test/java/org/gradle/fairy/tale/formula/ModularityTest.java
@@ -1,6 +1,7 @@
 package org.gradle.fairy.tale.formula;
 
 import org.gradle.actors.Actor;
+import org.gradle.actors.Imagination;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -10,10 +11,19 @@ import static org.junit.Assert.assertEquals;
  */
 public class ModularityTest {
     @Test
+    public void canReachActor() {
+        Actor actor = Imagination.createActor("Sean Connery");
+        assertEquals("Sean Connery", actor.toString());
+    }
+
+    /*
+    @Test
     public void canReachDefaultActor() {
+        // With Java 9 modules, this line now fails to compile.
         Actor actor = new org.gradle.actors.impl.DefaultActor("Elliot Ness");
         assertEquals("Elliot Ness", actor.toString());
     }
+    */
 
     /*
     @Test
