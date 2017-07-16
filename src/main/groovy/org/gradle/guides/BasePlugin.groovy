0f4d23cc90be5afe4519c77e1dab468b1a125c94
Benjamin Muschko <benjamin.muschko@gmail.com>
2017-07-16 18:36:36 -0400

    Add samples conventions
    Provides appropriate code and output directories as attributes to Asciidoctor. Defines samples directory to "test" task as system property for further processing.
    
    Uses the regular Apache 2 license across all files. No need to use an external plugin to enforce it. As needed FindBugs can be used to enforce the license header.

--- src/main/groovy/org/gradle/guides/BasePlugin.groovy
+++ src/main/groovy/org/gradle/guides/BasePlugin.groovy
@@ -1,16 +1,18 @@
-//
-// ============================================================================
-// (C) Copyright Schalk W. Cronje 2017
-//
-// This software is licensed under the Apache License 2.0
-// See http://www.apache.org/licenses/LICENSE-2.0 for license details
-//
-// Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and limitations under the License.
-//
-// ============================================================================
-//
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.gradle.guides
 
@@ -23,6 +25,15 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
 
+/**
+ * The guides base plugin provides conventions for all Gradle guides.
+ * <p>
+ * Adds the custom attributes to {@link org.asciidoctor.gradle.AsciidoctorTask} for reference in Asciidoc files:
+ * <ul>
+ *     <li>{@literal samplescodedir}: The directory containing samples code defined as {@literal "$projectDir/samples/code"}</li>
+ *     <li>{@literal samplescodedir}: The directory containing expected samples output defined as {@literal "$projectDir/samples/output"}</li>
+ * </ul>
+ */
 @CompileStatic
 class BasePlugin implements Plugin<Project> {
 
@@ -103,9 +114,11 @@ class BasePlugin implements Plugin<Project> {
                     'user-manual'        : "https://docs.gradle.org/${gradleVersion}/userguide/",
                     'language-reference' : "https://docs.gradle.org/${gradleVersion}/dsl/",
                     'api-reference'      : "https://docs.gradle.org/${gradleVersion}/javadoc/",
-                    'projdir'            : project.projectDir.absolutePath,
-                    'codedir'            : project.file('src/main').absolutePath,
-                    'testdir'            : project.file('src/test').absolutePath
+                    'projdir'            : project.projectDir,
+                    'codedir'            : project.file('src/main'),
+                    'testdir'            : project.file('src/test'),
+                    'samplescodedir'     : project.file('samples/code'),
+                    'samplesoutputdir'   : project.file('samples/output')
 
         }
 
