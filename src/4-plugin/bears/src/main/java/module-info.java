e40535f2c9a7ef40a8b6f70f8871c37a2395341a
Pepper Lebeck-Jobe <pepper@gradle.com>
2017-07-21 12:25:15 +0200

    Use "requires transitve" where appropriate
    Essentially, anytime gradle models a dependency as an `api`
    dependency, it the corresponding module should declare that it
    `requires transitively` the module it is consuming and rexposing some
    part of.

--- src/4-plugin/bears/src/main/java/module-info.java
+++ src/4-plugin/bears/src/main/java/module-info.java
@@ -1,9 +1,8 @@
 module org.gradle.fairy.tale.bears {
     requires org.gradle.actors;
-    requires org.gradle.fairy.tale;
+    requires transitive org.gradle.fairy.tale;
     requires org.gradle.fairy.tale.formula;
 
-    exports org.gradle.fairy.tale.bears;
     provides org.gradle.fairy.tale.Tale
         with org.gradle.fairy.tale.bears.GoldilocksAndTheThreeBears;
 }
