f50c3fe9b19119b45ead6220ef0722595bbd0388
Pepper Lebeck-Jobe <pepper@gradle.com>
2017-07-21 11:42:29 +0200

    Remove `export` declarations
    According to Alex Buckley at Oracle, it is an anti-pattern to expose
    public classes which are provided by the module using `provides` for
    consumption through non-ServiceLoader means.

--- src/3-application/pigs/src/main/java/module-info.java
+++ src/3-application/pigs/src/main/java/module-info.java
@@ -3,7 +3,6 @@ module org.gradle.fairy.tale.pigs {
     requires org.gradle.fairy.tale;
     requires org.gradle.fairy.tale.formula;
 
-    exports org.gradle.fairy.tale.pigs;
     provides org.gradle.fairy.tale.Tale
             with org.gradle.fairy.tale.pigs.ThreeLittlePigs;
 }
