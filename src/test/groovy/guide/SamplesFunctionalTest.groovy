be20452824d81162e39f7c61d7b8a886b0bffd89
Benjamin Muschko <benjamin.muschko@gmail.com>
2017-07-27 10:56:33 -0400

    Use test fixtures

--- src/test/groovy/guide/SamplesFunctionalTest.groovy
+++ src/test/groovy/guide/SamplesFunctionalTest.groovy
@@ -1,22 +1,13 @@
-import org.apache.commons.io.FileUtils
-import org.gradle.testkit.runner.BuildResult
-import org.gradle.testkit.runner.GradleRunner
-import org.junit.Rule
-import org.junit.rules.TemporaryFolder
-import spock.lang.Specification
+import org.gradle.guides.test.fixtures.AbstractSamplesFunctionalTest
 
-class SamplesFunctionalTest extends Specification {
-    private static final File SAMPLES_DIR = new File(System.getProperty('samplesDir'))
+class SamplesFunctionalTest extends AbstractSamplesFunctionalTest {
 
-    @Rule
-    TemporaryFolder temporaryFolder = new TemporaryFolder()
-    
     def "can execute convention over configuration sample"() {
         given:
-        copySample('convention-over-configuration')
+        copySampleCode('convention-over-configuration')
         
         when:
-        def result = build('deploy')
+        def result = succeeds('deploy')
         
         then:
         result.output.contains('Deploying to URL http://localhost:8080/server')
@@ -24,10 +15,10 @@ class SamplesFunctionalTest extends Specification {
     
     def "can execute capabilities vs. conventions sample"() {
         given:
-        copySample('capabilities-vs-conventions')
+        copySampleCode('capabilities-vs-conventions')
         
         when:
-        def result = build('tasks')
+        def result = succeeds('tasks')
         
         then:
         noExceptionThrown()
@@ -35,10 +26,10 @@ class SamplesFunctionalTest extends Specification {
     
     def "can execute Gradle API sample"() {
         given:
-        copySample('gradle-api')
+        copySampleCode('gradle-api')
         
         when:
-        build('classes')
+        succeeds('classes')
         
         then:
         noExceptionThrown()
@@ -46,25 +37,13 @@ class SamplesFunctionalTest extends Specification {
     
     def "can execute external libraries sample"() {
         given:
-        copySample('external-libraries')
+        copySampleCode('external-libraries')
         
         when:
-        def result = build('buildEnvironment')
+        def result = succeeds('buildEnvironment')
         
         then:
         result.output.contains("""classpath
 \\--- org.asciidoctor:asciidoctor-gradle-plugin:1.5.1""")
-    }
-    
-    private void copySample(String path) {
-        FileUtils.copyDirectory(new File(SAMPLES_DIR, "code/$path"), temporaryFolder.root)
-    }
-    
-    private BuildResult build(String... arguments) {
-        createAndConfigureGradleRunner(arguments).build()
-    }
-    
-    private GradleRunner createAndConfigureGradleRunner(String... arguments) {
-        GradleRunner.create().withProjectDir(temporaryFolder.root).withArguments(arguments).forwardOutput()
     }
 }
