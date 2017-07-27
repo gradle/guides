a678ae2f360620b419d1dbda6e17a903204df73c
Schalk W. Cronjé <schalk@gradle.com>
2017-07-27 15:42:19 +0200

    Added test to validate guide (gradle/guides#149)

---
+++ src/test/groovy/guides/ValidateSpec.groovy
@@ -0,0 +1,48 @@
+package guides
+
+import org.gradle.guides.test.fixtures.AbstractSamplesFunctionalTest
+import spock.lang.Unroll
+
+import java.nio.file.Files
+
+class ValidateSpec extends AbstractSamplesFunctionalTest {
+
+    static final File SRC_CODE_DIR   = new File( System.getProperty('samplesDir') ?: 'samples', 'code' )
+    static final File SRC_OUTPUT_DIR = new File( System.getProperty('samplesDir') ?: 'samples', 'output' )
+
+    void setup() {
+        initialize()
+    }
+
+    @Unroll
+    void 'Ensure migration example works: #source'() {
+        setup:
+        newBuildScript(source)
+        String expectedOutput = new File(SRC_OUTPUT_DIR,"${source}.txt").text
+
+        when:
+        String output = runGradle(task)
+
+        then:
+        output.contains expectedOutput
+
+        where:
+        source             | task
+        'bom'              | ['dependencies']
+        'provided'         | ['dependencies']
+        'processResources' | ['processResources']
+        'integTest'        | ['tasks']
+        'checkstyle'       | ['tasks','--all']
+        'ant'              | ['sayHello']
+
+    }
+
+    private void newBuildScript(final String source) {
+        Files.copy(new File(SRC_CODE_DIR,"${source}.gradle").toPath(),new File(testDirectory,'build.gradle').toPath())
+    }
+
+    private String runGradle(List<String> args) {
+        succeeds((String[])(args.toArray())).output.replaceAll( ~/Download.+?\n/,'')
+    }
+
+}
