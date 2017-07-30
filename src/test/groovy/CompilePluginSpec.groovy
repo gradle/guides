1b26a6643a74d8bf57836c24ed79bbc54815c3e3
Schalk W. Cronjé <schalk@gradle.com>
2017-07-30 18:33:29 +0200

    Incorporated feedback from @bmuschko (gradle/guides#149)

--- src/test/groovy/CompilePluginSpec.groovy
+++ src/test/groovy/CompilePluginSpec.groovy
@@ -1,34 +1,34 @@
 import org.apache.commons.io.FileUtils
+import org.gradle.guides.test.fixtures.AbstractSamplesFunctionalTest
 import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
 import org.gradle.testkit.runner.TaskOutcome
 import org.junit.Rule
 import org.junit.rules.TemporaryFolder
-import spock.lang.Specification
 
-class CompilePluginSpec extends Specification {
+class CompilePluginSpec extends AbstractSamplesFunctionalTest {
 
-    @Rule
-    TemporaryFolder workspace = new TemporaryFolder()
+    static final File SRC_CODE_DIR   = new File( System.getProperty('samplesDir') ?: 'samples', 'code' )
+    static final File SRC_OUTPUT_DIR = new File( System.getProperty('samplesDir') ?: 'samples', 'output' )
 
-    def 'Validate example code runs'() {
+    void setup() {
+        initialize()
+    }
+
+    void 'Validate example code runs'() {
 
         setup:
-        File output = new File(workspace.root,'output.txt')
-        FileUtils.copyDirectory( new File('src/example'), workspace.root)
+        FileUtils.copyDirectory( SRC_CODE_DIR, testDirectory)
 
         when:
-        BuildResult runner
-        output.withWriter { writer ->
-            runner = GradleRunner.create().
-                withArguments('hello').
-                forwardStdOutput(writer).
-                withProjectDir(workspace.root).
-                build()
-        }
+        String output = runGradle 'hello'
 
         then:
-        runner.task(':hello').outcome == TaskOutcome.SUCCESS
-        output.text.contains(':hello')
+        output.contains(':hello')
+        output.contains('Hello, World')
+    }
+
+    private String runGradle(String... args) {
+        succeeds(args).output.replaceAll( ~/Download.+?\n/,'')
     }
 }
