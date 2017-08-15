2bd78c68988020efc6d82c56db13b60dc9d51ad7
Benjamin Muschko <benjamin.muschko@gmail.com>
2017-08-15 12:55:23 -0400

    Improve tests

--- src/test/groovy/org/gradle/guides/SamplesFunctionalTest.groovy
+++ src/test/groovy/org/gradle/guides/SamplesFunctionalTest.groovy
@@ -4,16 +4,33 @@ import spock.lang.Unroll
 
 import org.gradle.guides.test.fixtures.AbstractSamplesFunctionalTest
 
+import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
+
 class SamplesFunctionalTest extends AbstractSamplesFunctionalTest {
+
+    def "can use composite builds"() {
+        given:
+        copySampleCode('include-plugin-build', 'consumer')
+        copySampleCode('url-verifier-plugin', 'url-verifier-plugin')
+        
+        when:
+        def result = gradleRunner.withProjectDir(new File(testDirectory, 'consumer')).withArguments('verifyUrl').build()
+        
+        then:
+        result.task(':verifyUrl').outcome == SUCCESS
+        result.output.contains("Successfully resolved URL 'http://www.google.com/'")
+    }
 
     def "can execute all tests in plugin project"() {
         given:
         copySampleCode('url-verifier-plugin')
         
         when:
-        succeeds('test')
+        def result = succeeds('check')
         
         then:
-        noExceptionThrown()
+        result.task(':test').outcome == SUCCESS
+        result.task(':integrationTest').outcome == SUCCESS
+        result.task(':functionalTest').outcome == SUCCESS
     }
 }
