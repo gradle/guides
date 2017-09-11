eb9807d3f400fbc526a21a57cd7c79f33f1f65c2
Eric Wendelin <eric@gradle.com>
2017-09-11 09:39:07 -0700

    Revert "Round reading time up to nearest multiple of 10"
    This reverts commit b4003e8679b536dfca7915d3b602706c3eaf1402.

--- js/set-time-to-complete-text.js
+++ js/set-time-to-complete-text.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var content = document.querySelector("#content");
 
     var NUMBER_OF_WORDS = content.innerText.split(" ").length;
-    var TIME_TO_COMPLETE = Math.ceil(Math.round(NUMBER_OF_WORDS / AVERAGE_WPM) / 10) * 10 + " minutes";
+    var TIME_TO_COMPLETE = (NUMBER_OF_WORDS < AVERAGE_WPM / 2) ? "1 minute" : Math.round(NUMBER_OF_WORDS / AVERAGE_WPM) + " minutes";
 
     var allTimeToCompleteTexts = document.querySelectorAll(".time-to-complete-text");
     [].forEach.call(allTimeToCompleteTexts, function(timeToCompleteText) {
