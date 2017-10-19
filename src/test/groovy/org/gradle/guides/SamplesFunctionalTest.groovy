73160d632f09f827c98da05dabe587a44b31b8e7
Benjamin Muschko <benjamin.muschko@gmail.com>
2017-10-19 09:45:55 -0600

    Externalize code, embed and test it

---
+++ src/test/groovy/org/gradle/guides/SamplesFunctionalTest.groovy
@@ -0,0 +1,36 @@
+package org.gradle.guides
+
+import spock.lang.Unroll
+
+import org.gradle.guides.test.fixtures.AbstractSamplesFunctionalTest
+
+import static org.gradle.testkit.runner.TaskOutcome.UP_TO_DATE
+
+class SamplesFunctionalTest extends AbstractSamplesFunctionalTest {
+
+    private static final String BUILD_SCAN_PUBLISHED_MESSAGE = 'Publishing build scan...'
+
+    def "can create build scan from build script"() {
+        given:
+        copySampleCode('build-scan-from-build-script')
+
+        when:
+        def result = succeeds('build', '--scan')
+
+        then:
+        result.task(':build').outcome == UP_TO_DATE
+        result.output.contains(BUILD_SCAN_PUBLISHED_MESSAGE)
+    }
+    
+    def "can create build scan from init script"() {
+        given:
+        copySampleCode('build-scan-from-init-script')
+
+        when:
+        def result = succeeds('build', '--scan', '-I', 'buildScan.gradle')
+
+        then:
+        result.task(':build').outcome == UP_TO_DATE
+        result.output.contains(BUILD_SCAN_PUBLISHED_MESSAGE)
+    }
+}
