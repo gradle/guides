7165823a56a87992db339f3f7ab1da6db1fc5660
Eric Wendelin <eric@gradle.com>
2018-03-06 15:24:04 -0700

    Convert build to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("com.gradle.build-scan") version "1.12.1"
+    id("org.gradle.guides.getting-started") version "0.11.2"
+    id("org.gradle.guides.test-jvm-code") version "0.11.2"
+}
+
+configure<org.gradle.guides.GuidesExtension> {
+    repoPath = "gradle-guides/migrating-from-maven"
+    mainAuthor = "Peter Ledbrook"
+}
+
+buildScan {
+    setLicenseAgreementUrl("https://gradle.com/terms-of-service")
+    setLicenseAgree("yes")
+    if (!System.getenv("CI").isNullOrEmpty()) {
+        publishAlways()
+        tag("CI")
+    }
+}
