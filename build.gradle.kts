33fa86aac4bd33660c244ccdf563c378dde26ca9
Eric Wendelin <eric@gradle.com>
2018-03-12 10:31:25 -0700

    Update gradle-guides plugins to v0.11.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.gradle.build-scan") version "1.12.1"
-    id("org.gradle.guides.getting-started") version "0.11.2"
-    id("org.gradle.guides.test-jvm-code") version "0.11.2"
+    id("org.gradle.guides.getting-started") version "0.11.5"
+    id("org.gradle.guides.test-jvm-code") version "0.11.5"
 }
 
 configure<org.gradle.guides.GuidesExtension> {
