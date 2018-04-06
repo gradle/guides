d53edd2ffa23fbb5160a2a0009a1806eac02f368
Eric Wendelin <eric@gradle.com>
2018-04-06 14:16:42 -0700

    Convert build to Kotlin and use build scans ftw

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("com.gradle.build-scan") version "1.12.1"
+    id("org.gradle.guides.getting-started") version "0.11.5"
+}
+
+configure<org.gradle.guides.GuidesExtension> {
+    repoPath = "gradle-guides/executing-gradle-builds-on-travisci"
+    mainAuthor = "Benjamin Muschko"
+}
+
+buildScan {
+    setLicenseAgreementUrl("https://gradle.com/terms-of-service")
+    setLicenseAgree("yes")
+    if (!System.getenv("CI").isNullOrEmpty()) {
+        publishAlways()
+        tag("CI")
+    }
+}
