0461d6dce64f362a4ee5cd63bd5b3a84489d077b
Benjamin Muschko <benjamin.muschko@gmail.com>
2018-04-11 17:30:42 -0600

    Clean up obsolete code and improve structure (#6)
    * Clean up obsolete code and improve structure
    * Use Kotlin DSL
    * Create build scan

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("org.gradle.guides.getting-started") version "0.11.5"
+
+    // Uncomment this line if you need test JVM code snippets
+    // id("org.gradle.guides.test-jvm-code") version "0.11.5"
+}
+
+configure<org.gradle.guides.GuidesExtension> {
+    repoPath = "gradle-guides/@@GUIDE_SLUG@@"
+    mainAuthor = "EDIT build.gradle TO ADD AUTHOR"
+}
+
+buildScan {
+    setLicenseAgreementUrl("https://gradle.com/terms-of-service")
+    setLicenseAgree("yes")
+    if (!System.getenv("CI").isNullOrEmpty()) {
+        publishAlways()
+        tag("CI")
+    }
+}
