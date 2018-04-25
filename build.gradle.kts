a5e43a685f89bdf64034ac8732bdce34038131cd
Paul Merlin <paul@gradle.com>
2018-04-25 11:51:52 +0200

    Initialize migrating-build-logic-from-groovy-to-kotlin repository (gradle/guides#180)

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("org.gradle.guides.topical") version "0.11.5"
+
+    // Uncomment this line if you need test JVM code snippets
+    // id("org.gradle.guides.test-jvm-code") version "0.11.5"
+}
+
+configure<org.gradle.guides.GuidesExtension> {
+    repoPath = "gradle-guides/migrating-build-logic-from-groovy-to-kotlin"
+    mainAuthor = "EDIT build.gradle TO ADD AUTHOR"
+}
+
+buildScan {
+    setLicenseAgreementUrl("https://gradle.com/terms-of-service")
+    setLicenseAgree("yes")
+    if (!System.getenv("CI").isNullOrEmpty()) {
+        publishAlways()
+        tag("CI")
+    }
+}
