e5e323ffca6fb7f00ddea25a2d1d8a8e4d541525
Paul Merlin <paul@gradle.com>
2018-05-02 15:14:20 +0200

    Upgrade Gradle Guides Plugin to 0.12.0
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     `build-scan`
-    id("org.gradle.guides.topical") version "0.11.5"
+    id("org.gradle.guides.topical") version "0.12.0"
 
     // Uncomment this line if you need test JVM code snippets
     // id("org.gradle.guides.test-jvm-code") version "0.11.5"
