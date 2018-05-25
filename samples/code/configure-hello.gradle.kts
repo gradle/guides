7949cd3c649b121d7096025ae4c09b1f2cd1b667
StefMa <StefMaDev@outlook.com>
2018-05-25 14:30:39 +0200

    Move import at top of file
    Signed-off-by: StefMa <StefMaDev@outlook.com>

--- samples/code/configure-hello.gradle.kts
+++ samples/code/configure-hello.gradle.kts
@@ -1,3 +1,8 @@
+// tag::configure-hello[]
+import org.example.greeting.Greeting
+
+// end::configure-hello[]
+
 // tag::use-plugin-id[]
 apply {
   plugin("org.example.greeting")
@@ -5,8 +10,6 @@ apply {
 // end::use-plugin-id[]
 
 // tag::configure-hello[]
-import org.example.greeting.Greeting
-
 tasks.getByName("hello", closureOf<Greeting> { // <1>
     message = "Hi"
     recipient = "Gradle"
