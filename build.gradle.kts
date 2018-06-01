4a4acb0ec9aa32c9d2699f7fe8fcf23fc790a9b1
Eric Wendelin <eric@gradle.com>
2018-06-01 11:22:59 -0700

    Update Gradle guides and build scans plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,27 @@
 plugins {
-    id("com.gradle.build-scan") version "1.13.1"
-    id("org.gradle.guides.getting-started") version "0.12.0"
-    id("org.gradle.guides.test-jvm-code") version "0.12.0"
+    groovy
+    id("com.gradle.build-scan") version "1.13.4"
+    id("org.gradle.guides.getting-started") version "0.13.2"
+    id("org.gradle.guides.test-jvm-code") version "0.13.2"
 }
 
 configure<org.gradle.guides.GuidesExtension> {
     repoPath = "gradle-guides/building-java-applications"
     mainAuthor = "Ken Kousen"
 }
+
+dependencies {
+    testCompile(localGroovy())
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_7
+    targetCompatibility = JavaVersion.VERSION_1_7
+}
+
 buildScan {
-    setLicenseAgreementUrl("https://gradle.com/terms-of-service")
-    setLicenseAgree("yes")
+    setTermsOfServiceUrl("https://gradle.com/terms-of-service")
+    setTermsOfServiceAgree("yes")
     if (!System.getenv("CI").isNullOrEmpty()) {
         publishAlways()
         tag("CI")
