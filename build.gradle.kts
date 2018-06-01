60122fda4ed097a777a081e249cafd4cdc73926b
Eric Wendelin <eric@gradle.com>
2018-06-01 13:00:01 -0700

    Migrate build to Kotlin DSL and adopt build scans

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("com.gradle.build-scan") version "1.13.4"
+    id("org.gradle.guides.topical") version "0.13.2"
+    id("org.gradle.guides.test-jvm-code") version "0.13.2"
+}
+
+configure<org.gradle.guides.GuidesExtension> {
+    repoPath = "gradle-guides/using-build-cache"
+}
+
+buildScan {
+    setLicenseAgreementUrl("https://gradle.com/terms-of-service")
+    setLicenseAgree("yes")
+    if (!System.getenv("CI").isNullOrEmpty()) {
+        publishAlways()
+        tag("CI")
+    }
+}
+
+tasks {
+    "asciidoctor"(org.asciidoctor.gradle.AsciidoctorTask::class) {
+        resources(delegateClosureOf<CopySpec> {
+            from("contents/css") {
+                into("css")
+            }
+            from("contents/images") {
+                into("images")
+            }
+        })
+    }
+}
