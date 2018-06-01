9c9f97e13a3acab0ebfb2da34b18dbea9bc86524
Eric Wendelin <eric@gradle.com>
2018-06-01 12:54:22 -0700

    Migrate build to Kotlin DSL and adopt build scans

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("com.gradle.build-scan") version "1.13.4"
+    id("org.gradle.guides.getting-started") version "0.13.2"
+    id("org.gradle.guides.test-jvm-code") version "0.13.2"
+}
+
+configure<org.gradle.guides.GuidesExtension> {
+    repoPath = "gradle-guides/writing-gradle-tasks"
+}
+
+buildScan {
+    setLicenseAgreementUrl("https://gradle.com/terms-of-service")
+    setLicenseAgree("yes")
+    if (!System.getenv("CI").isNullOrEmpty()) {
+        publishAlways()
+        tag("CI")
+    }
+}
+
