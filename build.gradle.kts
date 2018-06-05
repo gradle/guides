2693db3ccb0193c903e0a51887d2a254d123602a
Eric Wendelin <eric@gradle.com>
2018-06-04 17:39:54 -0700

    Upgrade to guides plugins 0.13.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     groovy
     id("com.gradle.build-scan") version "1.13.4"
-    id("org.gradle.guides.getting-started") version "0.13.2"
-    id("org.gradle.guides.test-jvm-code") version "0.13.2"
+    id("org.gradle.guides.getting-started") version "0.13.3"
+    id("org.gradle.guides.test-jvm-code") version "0.13.3"
 }
 
 configure<org.gradle.guides.GuidesExtension> {
