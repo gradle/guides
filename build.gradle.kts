407e93d08f04b894cee23b53ddf83145cf2cf6cf
Eric Wendelin <eric@gradle.com>
2018-06-04 17:39:58 -0700

    Upgrade to guides plugins 0.13.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
-    id("org.gradle.guides.topical") version "0.13.2"
+    id("org.gradle.guides.topical") version "0.13.3"
+    id("com.gradle.build-scan") version "1.13.4"
 
     // Uncomment this line if you need test JVM code snippets
-    // id("org.gradle.guides.test-jvm-code") version "0.13.2"
+    // id("org.gradle.guides.test-jvm-code") version "0.13.3"
 }
 
 configure<org.gradle.guides.GuidesExtension> {
