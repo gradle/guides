6e0bdacf0d5a305bcd7e1bc54afa1ba52e1cd749
Guy Brand <guy@gradle.com>
2018-07-11 21:52:47 +0200

    Add initial guide
    Signed-off-by: Guy Brand <guy@gradle.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,13 +4,24 @@ plugins {
     id("org.gradle.guides.test-jvm-code") version "0.13.3"
 }
 
+repositories {
+    maven {
+        url = uri("https://repo.gradle.org/gradle/libs")
+    }
+}
+
+dependencies {
+    implementation("org.gradle:sample-check:0.1.0")
+    testImplementation(gradleTestKit())
+}
+
 configure<org.gradle.guides.GuidesExtension> {
     repoPath = "gradle-guides/building-spring-boot-2-projects-with-gradle"
 }
 
 buildScan {
-    setLicenseAgreementUrl("https://gradle.com/terms-of-service")
-    setLicenseAgree("yes")
+    setTermsOfServiceUrl("https://gradle.com/terms-of-service")
+    setTermsOfServiceAgree("yes")
     if (!System.getenv("CI").isNullOrEmpty()) {
         publishAlways()
         tag("CI")
