a91f209d3d68e0ced262f4dc7bc524e0b45ed6c1
Guy Brand <guy@gradle.com>
2018-07-17 16:23:39 +0200

    Incorporate feedback
    - Remove Gradle application plugin
    - Make HelloGradleController top level class
    - Upgrade to Gradle 4.9
    
    Signed-off-by: Guy Brand <guy@gradle.com>

---
+++ samples/code/sample-project/src/main/java/hello/HelloGradleController.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class HelloGradleController {
+
+    @GetMapping
+    public String helloGradle() {
+        return "Hello Gradle!";
+    }
+
+}
