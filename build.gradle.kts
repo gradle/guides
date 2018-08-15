13b1c3901f78f5c24f7225981bc76a3c7f06416b
Eric Wendelin <eric@gradle.com>
2018-08-15 16:31:01 -0700

    Update Gradle and build-scan plugin to latest

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.gradle.build-scan") version "1.13.4"
+    id("com.gradle.build-scan") version "1.15.2"
     id("org.gradle.guides.getting-started") version "0.13.3"
     id("org.gradle.guides.test-jvm-code") version "0.13.3"
 }
