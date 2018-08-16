c8574e8b766fce39d1f90c223bf3442661554963
Paul Merlin <paul@gradle.com>
2018-08-16 21:16:36 +0200

    Move remaining samples to Examplar tests
    except the Android one that needs to skip when ANDROID_HOME is unset
    
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- samples/configurations-and-dependencies/imperative/kotlin/build.gradle.kts
+++ samples/configurations-and-dependencies/imperative/kotlin/build.gradle.kts
@@ -1,3 +1,4 @@
+// tag::delegated-properties[]
 apply(plugin = "java")
 val implementation by configurations
 val runtimeOnly by configurations
@@ -11,3 +12,16 @@ dependencies {
     }
     testRuntimeOnly("com.example:test-junit-jupiter-runtime:1.3")
 }
+// end::delegated-properties[]
+
+// tag::string-invoke[]
+apply(plugin = "java")
+dependencies {
+    "implementation"("com.example:lib:1.1")
+    "runtimeOnly"("com.example:runtime:1.0")
+    "testImplementation"("com.example:test-support:1.3") {
+        exclude(module = "junit")
+    }
+    "testRuntimeOnly"("com.example:test-junit-jupiter-runtime:1.3")
+}
+// end::string-invoke[]
--- src/test/groovy/org/gradle/guides/SamplesTest.groovy
+++ src/test/groovy/org/gradle/guides/SamplesTest.groovy
@@ -13,56 +13,19 @@ class SamplesTest extends AbstractSamplesFunctionalTest {
     }
 
     @Unroll
-    def "#section/#sample with #ext script"() {
-
-        setup:
-        copySampleCode("$section")
-
-        expect:
-        succeeds("help", "-b", "$sample$ext")
-
-        where:
-        section                           | sample                            | ext
-        "configuring-tasks"               | "basics"                          | ".gradle"
-        "configuring-tasks"               | "basics"                          | ".gradle.kts"
-        "configuring-tasks"               | "spring-boot"                     | ".gradle"
-        "configuring-tasks"               | "spring-boot"                     | ".gradle.kts"
-        "creating-tasks"                  | "project"                         | ".gradle"
-        "creating-tasks"                  | "project"                         | ".gradle.kts"
-        "creating-tasks"                  | "task-container-lazy"             | ".gradle"
-        "creating-tasks"                  | "task-container-lazy"             | ".gradle.kts"
-        "creating-tasks"                  | "task-container-eager"            | ".gradle"
-        "creating-tasks"                  | "task-container-eager"            | ".gradle.kts"
-        "creating-tasks"                  | "reference-lazy"                  | ".gradle"
-        "creating-tasks"                  | "reference-lazy"                  | ".gradle.kts"
-        "creating-tasks"                  | "reference-eager"                 | ".gradle"
-        "creating-tasks"                  | "reference-eager"                 | ".gradle.kts"
-        "configurations-and-dependencies" | "declarative"                     | ".gradle"
-        "configurations-and-dependencies" | "declarative"                     | ".gradle.kts"
-        "configurations-and-dependencies" | "imperative"                      | ".gradle"
-        "configurations-and-dependencies" | "imperative-string-reference"     | ".gradle.kts"
-        "configurations-and-dependencies" | "imperative-delegated-properties" | ".gradle.kts"
-        "configurations-and-dependencies" | "custom"                          | ".gradle"
-        "configurations-and-dependencies" | "custom"                          | ".gradle.kts"
-        "interoperability"                | "closureOf"                       | ".gradle.kts"
-        "interoperability"                | "delegateClosureOf"               | ".gradle.kts"
-        "interoperability"                | "withGroovyBuilder"               | ".gradle.kts"
-    }
-
-    @Unroll
     def "#section/#sample with #dsl android build"() {
 
         assumeThat(System.getenv('ANDROID_HOME'), notNullValue())
 
         setup:
-        copySampleCode("$section/$sample")
+        copySampleCode("../$section/$sample")
 
         expect:
         succeeds("help")
 
         where:
         section               | sample                | dsl
-        "declarative-scripts" | "declarative-android" | "groovy"
+        "declarative-scripts" | "declarative-android" | "kotlin"
         "declarative-scripts" | "imperative-android"  | "kotlin"
     }
 }
