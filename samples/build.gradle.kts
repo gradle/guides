1de670456ac731aad0d8a345e9b8b4c9ba076a3a
Marc Philipp <marc@gradle.com>
2018-09-06 12:20:31 +0200

    Simplify guide by using Gradle 4.10 features
    The guide now sets `description` and `displayName` for the published
    plugin in the `gradlePlugin` block. This makes using the `plugins`
    block from the `pluginBundle` extension unnecessary, in particular
    when only publishing a single plugin.
    
    Signed-off-by: Marc Philipp <marc@gradle.com>

--- samples/build.gradle.kts
+++ samples/build.gradle.kts
@@ -10,30 +10,18 @@ gradlePlugin {
     plugins { // <1>
         create("greetingsPlugin") { // <2>
             id = "<your plugin identifier>" // <3>
+            displayName = "<short displayable name for plugin>" // <4>
+            description = "<Good human-readable description of what your plugin is about>" // <5>
             implementationClass = "<your plugin class>"
         }
     }
 }
 // end::gradle-plugin[]
 
-// tag::plugin_bundle_global_information[]
+// tag::plugin_bundle[]
 pluginBundle {
     website = "<substitute your project website>" // <1>
     vcsUrl = "<uri to project source repository>" // <2>
-    tags = listOf("default", "tags", "unless", "overridden", "in", "plugin") // <3>
+    tags = listOf("tags", "for", "your", "plugins") // <3>
 }
-// end::plugin_bundle_global_information[]
-
-// tag::plugin_bundle_plugin_definitions[]
-pluginBundle {
-    (plugins) { // <1>
-        "greetingsPlugin" { // <2>
-            // id is captured from gradlePlugin extension block // <3>
-            displayName = "<short displayable name for plugin>" // <4>
-            description = "<Good human-readable description of what your plugin is about>" // <5>
-            tags = listOf("individual", "tags", "per", "plugin") // <6>
-            version = "1.2" // <7>
-        }
-    }
-}
-// end::plugin_bundle_plugin_definitions[]
+// end::plugin_bundle[]
