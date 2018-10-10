72bef4974dfb351b16391312200821e5dd696ce7
StefMa <StefMaDev@outlook.com>
2018-10-10 11:01:49 +0200

    Move plugin-dev-plugin to Kotlin
    Signed-off-by: StefMa <StefMaDev@outlook.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.asciidoctor.gradle.AsciidoctorTask
+
 plugins {
     id("org.gradle.guides.topical") version "0.14.1"
     id("org.gradle.guides.test-jvm-code") version "0.14.1"
@@ -5,4 +7,14 @@ plugins {
 
 guide {
     repoPath = "gradle-guides/implementing-gradle-plugins"
+}
+
+tasks {
+    getByName<AsciidoctorTask>("asciidoctor") {
+        inputs.dir("samples")
+        attributes(
+            mapOf("groovy-example-dir" to file("samples/groovy-dsl"),
+                  "kotlin-example-dir" to file("samples/kotlin-dsl"))
+        )
+    }
 }
