f208c11763a28cdcba9b42de4e71d922582c4c89
StefMa <StefMaDev@outlook.com>
2018-10-10 10:48:02 +0200

    Switch to the Kotlin DSL
    Signed-off-by: StefMa <StefMaDev@outlook.com>

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("org.gradle.guides.topical") version "0.14.0"
+    id("org.gradle.guides.test-jvm-code") version "0.14.0"
+}
+
+guide {
+    repoPath = "gradle-guides/implementing-gradle-plugins"
+}
