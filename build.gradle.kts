4fda3d5355459a8d6be92e69eef5b764fa20d406
StefMa <StefMaDev@outlook.com>
2018-10-10 10:49:17 +0200

    Upgrade applied plugins :rocket:
    Signed-off-by: StefMa <StefMaDev@outlook.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("org.gradle.guides.topical") version "0.14.0"
-    id("org.gradle.guides.test-jvm-code") version "0.14.0"
+    id("org.gradle.guides.topical") version "0.14.1"
+    id("org.gradle.guides.test-jvm-code") version "0.14.1"
 }
 
 guide {
