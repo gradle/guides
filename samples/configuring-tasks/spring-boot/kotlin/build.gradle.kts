2079e04b668cd02bf30aa871d8edafa951761437
Paul Merlin <paul@gradle.com>
2018-10-17 14:08:19 +0200

    Rework and simplify 'Configuring tasks'
    leveraging new task accessors
    
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- samples/configuring-tasks/spring-boot/kotlin/build.gradle.kts
+++ samples/configuring-tasks/spring-boot/kotlin/build.gradle.kts
@@ -1,40 +1,36 @@
 // tag::lazy[]
-// tag::eager[]
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 import org.springframework.boot.gradle.tasks.run.BootRun
 
+// tag::accessors[]
 plugins {
     java
     id("org.springframework.boot") version "2.0.2.RELEASE"
 }
 
 // end::lazy[]
-// end::eager[]
+// end::accessors[]
+
+// tag::accessors[]
+tasks.bootJar {
+    archiveName = "app.jar"
+    mainClassName = "com.example.demo.Demo"
+}
+
+tasks.bootRun {
+    main = "com.example.demo.Demo"
+    args("--spring.profiles.active=demo")
+}
+// end::accessors[]
 
 // tag::lazy[]
-tasks {
-    named<BootJar>("bootJar") {
-        archiveName = "app.jar"
-        mainClassName = "com.example.demo.Demo"
-    }
+tasks.named<BootJar>("bootJar") {
+    archiveName = "app.jar"
+    mainClassName = "com.example.demo.Demo"
+}
 
-    named<BootRun>("bootRun") {
-        main = "com.example.demo.Demo"
-        args("--spring.profiles.active=demo")
-    }
+tasks.named<BootRun>("bootRun") {
+    main = "com.example.demo.Demo"
+    args("--spring.profiles.active=demo")
 }
 // end::lazy[]
-
-// tag::eager[]
-tasks {
-    getByName<BootJar>("bootJar") {
-        archiveName = "app.jar"
-        mainClassName = "com.example.demo.Demo"
-    }
-
-    getByName<BootRun>("bootRun") {
-        main = "com.example.demo.Demo"
-        args("--spring.profiles.active=demo")
-    }
-}
-// end::eager[]
