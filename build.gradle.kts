9a35ec9ad86306aff1263822cf6c9cfe6ab09fff
Paul Merlin <paul@gradle.com>
2018-10-18 09:50:58 +0200

    Update guides plugin to 0.15.1
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,11 @@
 plugins {
     id("com.gradle.build-scan") version "1.15.2"
-    id("org.gradle.guides.getting-started") version "0.14.0"
-    id("org.gradle.guides.test-jvm-code") version "0.14.0"
+    id("org.gradle.guides.getting-started") version "0.15.1"
+    id("org.gradle.guides.test-jvm-code") version "0.15.1"
 }
 
 configure<org.gradle.guides.GuidesExtension> {
     repoPath = "gradle-guides/building-kotlin-jvm-libraries"
-}
-
-dependencies {
-    testImplementation("org.spockframework:spock-core:1.2-groovy-2.5")
 }
 
 buildScan {
