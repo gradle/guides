4d355734a15e58d9192b2ee8f2973ab5805cf234
Paul Merlin <paul@gradle.com>
2018-10-18 09:50:09 +0200

    Update guides plugin to 0.15.1
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,11 @@
 plugins {
     id("com.gradle.build-scan") version "1.15.2"
-    id("org.gradle.guides.topical") version "0.14.1"
-    id("org.gradle.guides.test-jvm-code") version "0.14.1"
+    id("org.gradle.guides.topical") version "0.15.1"
+    id("org.gradle.guides.test-jvm-code") version "0.15.1"
 }
 
 configure<org.gradle.guides.GuidesExtension> {
     repoPath = "gradle-guides/using-build-cache"
-}
-
-dependencies {
-    testImplementation("org.spockframework:spock-core:1.2-groovy-2.5")
 }
 
 buildScan {
