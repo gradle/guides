e86bef5e2561376040fa60f9002b839270cfcbeb
Eric Wendelin <eric@gradle.com>
2018-10-29 16:00:11 -0700

    Convert build to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("com.gradle.build-scan") version "2.0.1"
+    id("org.gradle.guides.getting-started") version "0.15.3"
+    id("org.ysb33r.vfs") version "1.0"
+}
+
+guide {
+    repoPath = "gradle-guides/building-cpp-libraries"
+}
+
+apply {
+    from("gradle/setup-googletest.gradle")
+    from("gradle/cpp.gradle")
+}
+
+buildScan {
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    termsOfServiceAgree = "yes"
+    if (!System.getenv("CI").isNullOrEmpty()) {
+        publishAlways()
+        tag("CI")
+    }
+}
