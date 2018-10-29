c2df9f8f703b0039a2d4ae90be54bb8b5f3c92fd
Eric Wendelin <eric@gradle.com>
2018-10-29 14:35:05 -0700

    Update header and footer to match Gradle 5.0 docs

--- src/functTest/groovy/org/gradle/guides/BasePluginFunctionalTest.groovy
+++ src/functTest/groovy/org/gradle/guides/BasePluginFunctionalTest.groovy
@@ -121,7 +121,7 @@ Hello world!
         then:
         def htmlFile = new File(temporaryFolder.root, 'build/html5/index.html').text
         htmlFile.contains('<script defer src="https://guides.gradle.org/js/guides')
-        htmlFile.contains('<header class="site-layout__header site-header" itemscope="itemscope" itemtype="https://schema.org/WPHeader">')
+        htmlFile.contains('<header class="site-layout__header site-header js-site-header" itemscope="itemscope" itemtype="https://schema.org/WPHeader">')
         htmlFile.contains('<footer class="site-layout__footer site-footer" itemscope="itemscope" itemtype="https://schema.org/WPFooter">')
     }
 
