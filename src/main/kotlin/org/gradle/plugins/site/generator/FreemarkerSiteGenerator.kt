e031f55c32c332851092d404d1613a633c754f67
Paul Merlin <paul@gradle.com>
2018-11-25 12:04:25 +0100

    Refine FreemarkerSiteGenerator
    Fix bad IO handling, closing streams and writers properly
    Prefer kotlin stdlib over FileUtils
    
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- src/main/kotlin/org/gradle/plugins/site/generator/FreemarkerSiteGenerator.kt
+++ src/main/kotlin/org/gradle/plugins/site/generator/FreemarkerSiteGenerator.kt
@@ -6,12 +6,9 @@ import freemarker.template.TemplateExceptionHandler
 import org.gradle.api.GradleException
 import org.gradle.plugins.site.data.ProjectDescriptor
 import org.gradle.plugins.site.data.ProjectLinksDescriptor
-import org.gradle.plugins.site.utils.FileUtils
 import java.io.File
-import java.io.FileWriter
 import java.io.IOException
 import java.io.InputStream
-import java.util.*
 
 /**
  * A site generator implementation based on [Freemarker](http://freemarker.org/).
@@ -31,45 +28,60 @@ class FreemarkerSiteGenerator(private val outputDir: File) : SiteGenerator {
 
     @Throws(IOException::class)
     private fun copyCssResources() {
-        val resources = ArrayList<String>()
-        resources.add("bootstrap.css")
-        resources.add("bootstrap-responsive.css")
+        val resources = listOf(
+                "bootstrap.css",
+                "bootstrap-responsive.css"
+        )
         copyResources("css", resources)
     }
 
     @Throws(IOException::class)
     private fun copyImgResources() {
-        val resources = ArrayList<String>()
-        resources.add("elephant-corner.png")
+        val resources = listOf(
+                "elephant-corner.png"
+        )
         copyResources("img", resources)
     }
 
     @Throws(IOException::class)
     private fun copyResources(subdir: String, resources: List<String>) {
-        val targetDir = File(outputDir, subdir)
-        FileUtils.createDirectory(targetDir)
-
+        val targetDir = outputDir.resolve(subdir)
         for (resource in resources) {
-            val sourcePath = "$subdir/$resource"
-            FileUtils.copyFile(resolveAsUrl(sourcePath), File(targetDir, resource))
+            resolveAsStream("$subdir/$resource").use { inputStream ->
+                inputStream.copyToFile(targetDir.resolve(resource))
+            }
         }
     }
 
-    private fun resolveAsUrl(name: String): InputStream {
-        return javaClass.classLoader.getResourceAsStream(name)
+    private fun resolveAsStream(name: String): InputStream {
+        return javaClass.classLoader.getResourceAsStream(name).buffered()
+    }
+
+    @Throws(IOException::class)
+    private fun InputStream.copyToFile(file: File) {
+        file.parentFile.let { dir ->
+            if (!dir.isDirectory && !dir.mkdirs()) throw IOException("Unable to create directory $dir")
+        }
+        file.outputStream().buffered().use { outputStream ->
+            copyTo(outputStream)
+        }
     }
 
     @Throws(IOException::class, TemplateException::class)
     private fun processIndexPageTemplate(projectDescriptor: ProjectDescriptor, linksDescriptor: ProjectLinksDescriptor) {
-        val cfg = Configuration(Configuration.VERSION_2_3_25)
-        cfg.setClassLoaderForTemplateLoading(javaClass.classLoader, "template")
-        cfg.defaultEncoding = "UTF-8"
-        cfg.templateExceptionHandler = TemplateExceptionHandler.RETHROW_HANDLER
-        cfg.logTemplateExceptions = false
-        val root = HashMap<String, Any>()
-        root["project"] = projectDescriptor
-        root["customData"] = linksDescriptor
+        val cfg = Configuration(Configuration.VERSION_2_3_25).apply {
+            setClassLoaderForTemplateLoading(javaClass.classLoader, "template")
+            defaultEncoding = "UTF-8"
+            templateExceptionHandler = TemplateExceptionHandler.RETHROW_HANDLER
+            logTemplateExceptions = false
+        }
+        val root = mapOf(
+                "project" to projectDescriptor,
+                "customData" to linksDescriptor
+        )
         val template = cfg.getTemplate("index.ftl")
-        template.process(root, FileWriter(File(outputDir, "index.html")))
+        outputDir.resolve("index.html").bufferedWriter().use { writer ->
+            template.process(root, writer)
+        }
     }
 }
