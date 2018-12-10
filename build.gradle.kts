e61dffae8d2e4c205a523e128874fb83a8ec4bdf
Tony Robalik <tony.robalik@gmail.com>
2018-12-10 14:23:34 -0800

    Added section for Caching Android projects.
    Signed-off-by: Tony Robalik <tony@gradle.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.gradle.build-scan") version "1.15.2"
     id("org.gradle.guides.topical") version "0.14.0"
     id("org.gradle.guides.test-jvm-code") version "0.14.0"
+    kotlin("kapt") version "1.3.11" apply false
 }
 
 configure<org.gradle.guides.GuidesExtension> {
