cc1e5a8991a8a797b96710a46615db10e54d6d54
Daniel Lacasse <daniel@gradle.com>
2019-01-15 12:33:36 +0100

    Revert `getting-started` plugin to 0.15.0
    Signed-off-by: Daniel Lacasse <daniel@gradle.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.gradle.build-scan") version "2.0.2"
-    id("org.gradle.guides.getting-started") version "0.15.5"
+    id("org.gradle.guides.getting-started") version "0.15.0"
 }
 
 guide {
