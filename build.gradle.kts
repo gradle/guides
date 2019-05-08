4496b3efed00d3efd7d3f73b68e4fcfb24e532e6
Daniel Lacasse <daniel@gradle.com>
2019-05-08 12:00:10 +0200

    Patch failure on macOS Travis CI
    Signed-off-by: Daniel Lacasse <daniel@gradle.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,13 @@ apply {
     from("gradle/cpp.gradle")
 }
 
+// NOTE: Patch until we fix this in guide plugin
+afterEvaluate {
+    tasks.named("gitPublishReset") {
+        enabled = true
+    }
+}
+
 buildScan {
     setTermsOfServiceUrl("https://gradle.com/terms-of-service")
     setTermsOfServiceAgree("yes")
