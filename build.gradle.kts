84fa54864948316994d2e7dba8a07fde04b9de66
Daniel Lacasse <daniel@gradle.com>
2019-05-08 08:52:58 +0200

    Update Gradle wrapper to 5.4.1
    Signed-off-by: Daniel Lacasse <daniel@gradle.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.gradle.build-scan") version "1.15.2"
+    id("com.gradle.build-scan") version "2.3"
     id("org.gradle.guides.topical") version "0.14.0"
     id("org.gradle.guides.test-jvm-code") version "0.14.0"
 }
