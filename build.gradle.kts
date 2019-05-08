5ef746ecc1cf8fb199007f391d2cb828edcd2bed
Daniel Lacasse <daniel@gradle.com>
2019-05-08 18:17:34 +0200

    Update the guide code to fix CI failures
    Signed-off-by: Daniel Lacasse <daniel@gradle.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.gradle.guides.tutorial") version "0.14.0"
+    id("org.gradle.guides.tutorial") version "0.15.5"
     id("com.gradle.build-scan") version "2.3"
 
     // Uncomment this line if you need test JVM code snippets
