73f2aa82cbcf8d23f12b500d5a5eac130d5d0221
Daniel Lacasse <daniel@gradle.com>
2019-05-10 10:48:54 +0200

    Resolve the guides plugin to the included build
    Signed-off-by: Daniel Lacasse <daniel@gradle.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,15 @@
 file(".mrconfig").readLines().filter { it.startsWith("[") && it.endsWith("]") }.map { it.substring(1, it.length-1) }.forEach {
     settings.includeBuild(it)
 }
+
+// Resolve to the gradle-guides-plugin subprojects instead of the released plugins
+val guidePluginIds = setOf("org.gradle.guides.base", "org.gradle.guides.getting-started", "org.gradle.guides.topical", "org.gradle.guides.tutorial", "org.gradle.guides.test-jvm-code")
+pluginManagement {
+    resolutionStrategy {
+        eachPlugin {
+            if (guidePluginIds.contains(requested.id.id)) {
+                useModule("org.gradle.guides:gradle-guides-plugin")
+            }
+        }
+    }
+}
