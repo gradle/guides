d22858fa671786a0651dd938664b13251c5da96d
Daniel Lacasse <daniel@gradle.com>
2019-05-21 14:42:18 +0200

    Skip Gradle multi-project is mr didn't checkout
    Signed-off-by: Daniel Lacasse <daniel@gradle.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,16 @@
-file(".mrconfig").readLines().filter { it.startsWith("[") && it.endsWith("]") }.map { it.substring(1, it.length-1) }.forEach {
-    settings.includeBuild(it)
-}
+if (file("subprojects").exists()) {
+    file(".mrconfig").readLines().filter { it.startsWith("[") && it.endsWith("]") }.map { it.substring(1, it.length - 1) }.forEach {
+        settings.includeBuild(it)
+    }
 
 // Resolve to the gradle-guides-plugin subprojects instead of the released plugins
-val guidePluginIds = setOf("org.gradle.guides.base", "org.gradle.guides.getting-started", "org.gradle.guides.topical", "org.gradle.guides.tutorial", "org.gradle.guides.test-jvm-code")
-pluginManagement {
-    resolutionStrategy {
-        eachPlugin {
-            if (guidePluginIds.contains(requested.id.id)) {
-                useModule("org.gradle.guides:gradle-guides-plugin")
+    val guidePluginIds = setOf("org.gradle.guides.base", "org.gradle.guides.getting-started", "org.gradle.guides.topical", "org.gradle.guides.tutorial", "org.gradle.guides.test-jvm-code")
+    pluginManagement {
+        resolutionStrategy {
+            eachPlugin {
+                if (guidePluginIds.contains(requested.id.id)) {
+                    useModule("org.gradle.guides:gradle-guides-plugin")
+                }
             }
         }
     }
