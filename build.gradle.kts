3ca6abbc69b156c5208bdaf071fe44175d4e1785
Daniel Lacasse <daniel@gradle.com>
2019-05-29 01:14:33 +0400

    Add minimum Gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,13 @@
 plugins {
     id("com.gradle.build-scan") version "2.3"
-    id("org.gradle.guides.getting-started") version "0.15.5"
-    id("org.gradle.guides.test-jvm-code") version "0.15.5"
+    id("org.gradle.guides.getting-started") version "0.15.7"
+    id("org.gradle.guides.test-jvm-code") version "0.15.7"
 }
 
 configure<org.gradle.guides.GuidesExtension> {
     repoPath = "gradle-guides/migrating-from-maven"
     mainAuthor = "Peter Ledbrook"
+    minimumGradleVersion.set("4.10.3")
 }
 
 buildScan {
