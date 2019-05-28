964962f96e1aba43ee454a65450907805621898b
Daniel Lacasse <daniel@gradle.com>
2019-05-29 01:14:31 +0400

    Add minimum Gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     id("com.gradle.build-scan") version "2.3"
-    id("org.gradle.guides.getting-started") version "0.15.5"
+    id("org.gradle.guides.getting-started") version "0.15.7"
 }
 
 configure<org.gradle.guides.GuidesExtension> {
     repoPath = "gradle-guides/building-cpp-executables"
+    minimumGradleVersion.set("5.2.1")
 }
 
 apply {
