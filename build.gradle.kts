7f0b3c806f04ef6c8266eaa2f1840921e3391ab4
Daniel Lacasse <daniel@gradle.com>
2019-05-29 01:14:33 +0400

    Add minimum Gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     id("com.gradle.build-scan") version "2.3"
-    id("org.gradle.guides.topical") version "0.15.5"
-    id("org.gradle.guides.test-jvm-code") version "0.15.5"
+    id("org.gradle.guides.topical") version "0.15.7"
+    id("org.gradle.guides.test-jvm-code") version "0.15.7"
 }
 
 configure<org.gradle.guides.GuidesExtension> {
     repoPath = "gradle-guides/performance"
+    minimumGradleVersion.set("4.10.3")
 }
 
 buildScan {
