865c5bcd617aea15cbe0c7690ad07b86f5f0dacf
Daniel Lacasse <daniel@gradle.com>
2019-05-29 01:14:34 +0400

    Add minimum Gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     id("com.gradle.build-scan") version "2.3"
-    id("org.gradle.guides.getting-started") version "0.15.5"
-    id("org.gradle.guides.test-jvm-code") version "0.15.5"
+    id("org.gradle.guides.getting-started") version "0.15.7"
+    id("org.gradle.guides.test-jvm-code") version "0.15.7"
 }
 
 configure<org.gradle.guides.GuidesExtension> {
     repoPath = "gradle-guides/using-the-worker-api"
+    minimumGradleVersion.set("4.10.3")
 }
 
 buildScan {
