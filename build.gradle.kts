29f6c7226a4fb8eda2a8c37dc37031af3c4e246d
Daniel Lacasse <daniel@gradle.com>
2019-05-29 01:14:34 +0400

    Add minimum Gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,15 @@
 plugins {
-    id("org.gradle.guides.topical") version "0.15.5"
+    id("org.gradle.guides.topical") version "0.15.7"
     id("com.gradle.build-scan") version "2.3"
 
     // Uncomment this line if you need test JVM code snippets
-    // id("org.gradle.guides.test-jvm-code") version "0.14.0"
+    // id("org.gradle.guides.test-jvm-code") version "0.15.7"
 }
 
 configure<org.gradle.guides.GuidesExtension> {
     repoPath = "gradle-guides/@@GUIDE_SLUG@@"
-    mainAuthor = "EDIT build.gradle TO ADD AUTHOR"
+    mainAuthor = "EDIT build.gradle.kts TO ADD AUTHOR"
+    minimumGradleVersion.set("EDIT build.gradle.kts TO ADD MINIMUM GRADLE VERSION")
 }
 
 buildScan {
