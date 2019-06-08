dab7a9996bd5caaa20d9d5fb6c96f06eb1882964
Daniel Lacasse <daniel@gradle.com>
2019-06-08 16:44:12 +0200

    Make the guide Kotlin DSL uniform

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("org.gradle.guides.getting-started") version "0.15.10"
 }
 
-configure<org.gradle.guides.GuidesExtension> {
+guide {
     repositoryPath.set("gradle-guides/executing-gradle-builds-on-teamcity")
     minimumGradleVersion.set("4.10.3")
     title.set("Executing Gradle builds on TeamCity")
