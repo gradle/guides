55a78020f527c4cf8c5fdf301819eefd5ac942d9
Daniel Lacasse <daniel@gradle.com>
2019-06-08 13:47:07 +0200

    Remove mainAuthor property configuration from all guides

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 
 configure<org.gradle.guides.GuidesExtension> {
     repoPath = "gradle-guides/@@GUIDE_SLUG@@"
-    mainAuthor = "EDIT build.gradle.kts TO ADD AUTHOR"
     minimumGradleVersion.set("EDIT build.gradle.kts TO ADD MINIMUM GRADLE VERSION")
 }
 
