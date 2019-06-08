cbf843c643216aedb44cdf83e73c41d9b5b2d042
Daniel Lacasse <daniel@gradle.com>
2019-06-08 16:25:25 +0200

    Configure root project name and guide title

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 configure<org.gradle.guides.GuidesExtension> {
     repositoryPath.set("gradle-guides/executing-gradle-builds-on-teamcity")
     minimumGradleVersion.set("4.10.3")
+    title.set("Executing Gradle builds on TeamCity")
 }
 
 buildScan {
