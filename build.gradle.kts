45da05dad118bbe945c65a712e81b2290c27402a
Daniel Lacasse <daniel@gradle.com>
2019-06-08 17:24:48 +0200

    Upgrade guide plugins to version 0.15.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.gradle.build-scan") version "2.3"
-    id("org.gradle.guides.getting-started") version "0.15.10"
-    id("org.gradle.guides.test-jvm-code") version "0.15.10"
+    id("org.gradle.guides.getting-started") version "0.15.11"
+    id("org.gradle.guides.test-jvm-code") version "0.15.11"
 }
 
 guide {
