be97d783866f3313624f5b45311f96c31929fe2b
Daniel Lacasse <daniel@gradle.com>
2019-06-08 16:44:12 +0200

    Make the guide Kotlin DSL uniform

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,12 @@ plugins {
     id("org.gradle.guides.test-jvm-code") version "0.15.10"
 }
 
+guide {
+    repositoryPath.set("gradle-guides/building-spring-boot-2-projects-with-gradle")
+    minimumGradleVersion.set("4.10.3")
+    title.set("Building Spring Boot 2 Applications with Gradle")
+}
+
 repositories {
     maven {
         url = uri("https://repo.gradle.org/gradle/libs")
@@ -16,12 +22,6 @@ dependencies {
     }
     testImplementation("org.gradle:sample-check:0.5.0")
     testImplementation(gradleTestKit())
-}
-
-configure<org.gradle.guides.GuidesExtension> {
-    repositoryPath.set("gradle-guides/building-spring-boot-2-projects-with-gradle")
-    minimumGradleVersion.set("4.10.3")
-    title.set("Building Spring Boot 2 Applications with Gradle")
 }
 
 buildScan {
