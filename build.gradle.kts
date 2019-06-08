e22745ca6511e5254abd055eb07fbb554969b8ae
Daniel Lacasse <daniel@gradle.com>
2019-06-08 16:44:13 +0200

    Make the guide Kotlin DSL uniform

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("org.gradle.guides.test-jvm-code") version "0.15.10"
 }
 
-configure<org.gradle.guides.GuidesExtension> {
+guide {
     repositoryPath.set("gradle-guides/performance")
     minimumGradleVersion.set("4.10.3")
     title.set("Improving the Performance of Gradle Builds")
