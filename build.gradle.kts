531c1903dd21def0504818860be70527ac2bcfc6
Daniel Lacasse <daniel@gradle.com>
2019-06-08 16:25:26 +0200

    Configure root project name and guide title

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
 configure<org.gradle.guides.GuidesExtension> {
     repositoryPath.set("gradle-guides/running-webpack-with-gradle")
     minimumGradleVersion.set("4.10.3")
+    title.set("Running Webpack with Gradle")
 }
 
 buildScan {
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "running-webpack-with-gradle"
