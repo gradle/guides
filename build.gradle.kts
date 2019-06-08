eabbd5775adb30665722e4d09cf850f7b3e68ca8
Daniel Lacasse <daniel@gradle.com>
2019-06-08 16:44:11 +0200

    Make the guide Kotlin DSL uniform

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("org.gradle.guides.test-jvm-code") version "0.15.10"
 }
 
-configure<org.gradle.guides.GuidesExtension> {
+guide {
     repositoryPath.set("gradle-guides/building-kotlin-jvm-libraries")
     minimumGradleVersion.set("5.0")
     title.set("Building Kotlin JVM Libraries")
