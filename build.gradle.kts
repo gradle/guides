985b17c31c6a4ed4f21dab70ebe1e9fc4424bd1b
Daniel Lacasse <daniel@gradle.com>
2019-06-08 16:25:24 +0200

    Configure root project name and guide title

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
 configure<org.gradle.guides.GuidesExtension> {
     repositoryPath.set("gradle-guides/building-spring-boot-2-projects-with-gradle")
     minimumGradleVersion.set("4.10.3")
+    title.set("Building Spring Boot 2 Applications with Gradle")
 }
 
 buildScan {
