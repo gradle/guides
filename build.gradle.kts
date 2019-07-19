73b98346658e32f5c9a8ded37fb165fe7b6aae9e
Daniel Lacasse <daniel@gradle.com>
2019-07-20 00:09:20 +0400

    Update guides plugins to 0.15.13

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import org.asciidoctor.gradle.AsciidoctorTask
 
 plugins {
     `build-scan`
-    id("org.gradle.guides.topical") version "0.15.12"
-    id("org.gradle.guides.test-jvm-code") version "0.15.12"
+    id("org.gradle.guides.topical") version "0.15.13"
+    id("org.gradle.guides.test-jvm-code") version "0.15.13"
 }
 
 guide {
