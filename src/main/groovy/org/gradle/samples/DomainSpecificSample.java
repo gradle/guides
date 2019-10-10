839cb5840487525d8246a37805c7a9261e6bc511
Daniel Lacasse <daniel@gradle.com>
2019-10-10 18:32:34 +0200

    Document the public API of the sample plugin

--- src/main/groovy/org/gradle/samples/DomainSpecificSample.java
+++ src/main/groovy/org/gradle/samples/DomainSpecificSample.java
@@ -3,5 +3,10 @@ package org.gradle.samples;
 import org.gradle.api.file.ConfigurableFileCollection;
 
 public interface DomainSpecificSample {
+    /**
+     * Property for configuring the content of single domain specific archive.
+     *
+     * @return  a file collection to configure the files to include in the domain specific archive
+     */
     ConfigurableFileCollection getArchiveContent();
 }
--- src/main/groovy/org/gradle/samples/Sample.java
+++ src/main/groovy/org/gradle/samples/Sample.java
@@ -9,22 +9,74 @@ import org.gradle.api.plugins.ExtensionAware;
 import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.TaskProvider;
 
+/**
+ * Represent a sample to be documented. Each sample must contains a Groovy or Kotlin DSL sample.
+ */
 public interface Sample extends Named, ExtensionAware {
+    /**
+     * Property for configuring the sample root directory.
+     *
+     * @return a property for the sample root directory
+     */
     DirectoryProperty getSampleDirectory();
 
+    /**
+     * Property for configuring the sample description. The description is used within the sample index.
+     *
+     * @return a property for the sample description
+     */
     Property<String> getDescription();
 
+    /**
+     * Property for configuring the Gradle version wrapper to generate for this sample.
+     *
+     * @return a property for the Gradle version wrapper to generate
+     */
     Property<String> getGradleVersion();
 
+    /**
+     * Property for configuring the common content to include in both domain specific sample archives.
+     *
+     * @return a file collection to configure the files to include in the domain specific archives
+     */
     ConfigurableFileCollection getArchiveContent();
 
+    /**
+     * Explicitly declare a Groovy DSL sample at the conventional location (e.g. {@code src/samples/<name>/groovy}).
+     *
+     * NOTE: Explicitly declaring a domain specific sample exclude any implicit conventional declaration.
+     */
     void withGroovyDsl();
 
+    /**
+     * Explicitly declare a Groovy DSL sample at a custom location.
+     *
+     * NOTE: Explicitly declaring a domain specific sample exclude any implicit conventional declaration.
+     *
+     * @param action the configuration action for the Groovy DSL
+     */
     void withGroovyDsl(Action<? super DomainSpecificSample> action);
 
+    /**
+     * Explicitly declare a Kotlin DSL sample at the conventional location (e.g. {@code src/samples/<name>/kotlin}).
+     *
+     * NOTE: Explicitly declaring a domain specific sample exclude any implicit conventional declaration.
+     */
     void withKotlinDsl();
 
+    /**
+     * Explicitly declare a Kotlin DSL sample at a custom location.
+     *
+     * NOTE: Explicitly declaring a domain specific sample exclude any implicit conventional declaration.
+     *
+     * @param action the configuration action for the Kotlin DSL
+     */
     void withKotlinDsl(Action<? super DomainSpecificSample> action);
 
+    /**
+     * Returns the Asciidoctor task that process the README.adoc file.
+     *
+     * @return a provider for the Asciidoctor task of the sample README.adoc
+     */
     TaskProvider<AsciidoctorTask> getAsciidoctorTask();
 }
