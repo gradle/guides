d30894388ef850a1a9db7695105ecef1f53d1b0c
Daniel Lacasse <daniel@gradle.com>
2019-11-13 16:57:29 +0400

    Try to detect binary files and avoid filtering them

--- src/main/groovy/org/gradle/samples/internal/tasks/InstallSampleZipContentTask.java
+++ src/main/groovy/org/gradle/samples/internal/tasks/InstallSampleZipContentTask.java
@@ -4,17 +4,19 @@ import org.apache.tools.ant.filters.LineContainsRegExp;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.file.ConfigurableFileCollection;
 import org.gradle.api.file.DirectoryProperty;
-import org.gradle.api.file.FileTree;
 import org.gradle.api.tasks.InputFiles;
 import org.gradle.api.tasks.OutputDirectory;
 import org.gradle.api.tasks.TaskAction;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FilterReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Reader;
+import java.io.UncheckedIOException;
 
 public abstract class InstallSampleZipContentTask extends DefaultTask {
-    private static final String WRAPPER_FILES_PATTERN = "gradle/**/*";
-
     @InputFiles
     public abstract ConfigurableFileCollection getSource();
 
@@ -24,16 +26,32 @@ public abstract class InstallSampleZipContentTask extends DefaultTask {
     @TaskAction
     private void doInstall() {
         getProject().sync(spec -> {
-            FileTree nonWrapperFiles = getSource().getAsFileTree().matching(it -> it.exclude(WRAPPER_FILES_PATTERN));
-            spec.from(nonWrapperFiles, it -> it.filter(AsciidoctorTagsFilter.class));
-
-            FileTree wrapperFiles = getSource().getAsFileTree().matching(it -> it.include(WRAPPER_FILES_PATTERN));
-            spec.from(wrapperFiles);
+            spec.from(getSource());
 
             spec.exclude("**/build/**", "**/.gradle/**");
+            spec.eachFile(details -> {
+                if (!isBinaryFile(details.getFile())) {
+                    details.filter(AsciidoctorTagsFilter.class);
+                }
+            });
 
             spec.into(getInstallDirectory());
         });
+    }
+
+    private static boolean isBinaryFile(File file) {
+        try (InputStream inStream = new FileInputStream(file)) {
+            byte[] buffer = new byte[8192];
+            int nread = inStream.read(buffer);
+            for (int i = 0; i < nread; ++i) {
+                if (buffer[i] == 0) {
+                    return true;
+                }
+            }
+            return false;
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 
     public static class AsciidoctorTagsFilter extends FilterReader {
