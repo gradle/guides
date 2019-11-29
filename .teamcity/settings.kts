b8b00eae4247584cbd8fe565aad8caa5d2c301e2
Daniel Lacasse <daniel@gradle.com>
2019-11-29 22:19:45 +0400

    Initial configuration for Teamcity

---
+++ .teamcity/settings.kts
@@ -0,0 +1,70 @@
+import jetbrains.buildServer.configs.kotlin.v2018_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.v2018_2.project
+import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.GitVcsRoot
+import jetbrains.buildServer.configs.kotlin.v2018_2.version
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2019.1"
+
+project {
+    uuid = "4175c92d-44cc-4a08-9e68-67d805a9dc7d"
+    id("GradleGuidesPlugin")
+    name = "Gradle Guides Plugin"
+
+    buildType(Build)
+}
+
+object Build : BuildType({
+    name = "Build"
+
+    vcs {
+        root(GradleGuidesPluginBranches)
+    }
+    steps {
+        gradle {
+            useGradleWrapper = true
+            tasks = "build"
+        }
+    }
+    triggers {
+        vcs {
+            groupCheckinsByCommitter = true
+        }
+    }
+})
+
+object GradleGuidesPluginBranches : GitVcsRoot({
+    uuid = "022ee0e9-d93c-4b47-acbe-2b476820aa41"
+    name = "Gradle Guides Plugin Branches"
+    url = "https://github.com/gradle-guides/gradle-guides-plugin.git"
+    branchSpec = "+:refs/heads/*"
+    agentGitPath = "%env.TEAMCITY_GIT_PATH%"
+    useMirrors = false
+    authMethod = password {
+        userName = "gradlewaregitbot"
+        password = "credentialsJSON:5306bfc7-041e-46e8-8d61-1d49424e7b04"
+    }
+})
