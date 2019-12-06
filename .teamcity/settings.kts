c66020691845329e9e9607e5a5767250d802e7e7
Daniel Lacasse <daniel@gradle.com>
2019-12-06 16:12:39 +0400

    Use the right publishing task on TeamCity

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -74,7 +74,7 @@ object Publish : AbstractBuild({
         gradle {
             useGradleWrapper = true
             gradleParams = "-Dgradle.publish.skip.namespace.check=true -Pgradle.publish.key=%GRADLE_PUBLISH_KEY% -Pgradle.publish.secret=%GRADLE_PUBLISH_SECRET%"
-            tasks = "publish"
+            tasks = "publishPlugins"
         }
     }
     triggers {
