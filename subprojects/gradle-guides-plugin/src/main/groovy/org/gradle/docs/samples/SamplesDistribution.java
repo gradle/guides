2d22e6a6175df605c95bd4e7bf67ca940ceebee3
Daniel Lacasse <daniel@gradle.com>
2019-12-26 15:46:17 +0400

    Convert SamplesDistribution to interface

--- subprojects/gradle-guides-plugin/src/main/groovy/org/gradle/docs/samples/SamplesDistribution.java
+++ subprojects/gradle-guides-plugin/src/main/groovy/org/gradle/docs/samples/SamplesDistribution.java
@@ -6,27 +6,27 @@ import org.gradle.api.file.ConfigurableFileTree;
 /**
  * This represents the outputs of the samples section of the documentation.
  */
-public abstract class SamplesDistribution {
+public interface SamplesDistribution {
 
     /**
      * @return the collection of samples extracted as a consumer would see them
      */
-    public abstract ConfigurableFileTree getInstalledSamples();
+    ConfigurableFileTree getInstalledSamples();
 
     /**
      * @return a collection of samples in zip form
      */
-    public abstract ConfigurableFileCollection getZippedSamples();
+    ConfigurableFileCollection getZippedSamples();
 
     /**
      * @return Collection of samples ready for testing
      */
-    public abstract ConfigurableFileTree getTestedInstalledSamples();
+    ConfigurableFileTree getTestedInstalledSamples();
 
     /**
      * This is HTML and resources.
      *
      * @return collection of rendered documentation
      */
-    public abstract ConfigurableFileCollection getRenderedDocumentation();
+    ConfigurableFileCollection getRenderedDocumentation();
 }
