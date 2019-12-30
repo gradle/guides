d9209c75f3f53e596f0e782f78d7b3a0fcbce43a
Daniel Lacasse <daniel@gradle.com>
2019-12-30 10:28:36 +0400

    Use general asciidoctor attributes

--- subprojects/writing-gradle-tasks/build.gradle.kts
+++ subprojects/writing-gradle-tasks/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.asciidoctor.gradle.AsciidoctorTask
-
 plugins {
     id("org.gradle.guides")
 }
@@ -8,21 +6,4 @@ guide {
     repositoryPath.set("gradle-guides/writing-gradle-tasks")
     minimumGradleVersion.set("5.0")
     category.set("Getting Started")
-}
-
-tasks {
-    getByName<AsciidoctorTask>("asciidoctor") {
-        inputs.dir("samples")
-        attributes(
-            mapOf("groovy-example-dir" to file("samples/groovy-dsl"),
-                  "kotlin-example-dir" to file("samples/kotlin-dsl"))
-        )
-    }
-    getByName<AsciidoctorTask>("guidesMultiPage") {
-        inputs.dir("samples")
-        attributes(
-                mapOf("groovy-example-dir" to file("samples/groovy-dsl"),
-                        "kotlin-example-dir" to file("samples/kotlin-dsl"))
-        )
-    }
 }
