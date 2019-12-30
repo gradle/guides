95abec5bf70d4bc09f4ac1a28f3bc899916f17fc
Daniel Lacasse <daniel@gradle.com>
2019-12-30 09:39:44 +0400

    Remove unecessary configuration for guide

--- subprojects/building-java-applications/build.gradle.kts
+++ subprojects/building-java-applications/build.gradle.kts
@@ -7,20 +7,3 @@ guide {
     minimumGradleVersion.set("5.4.1")
     category.set("Getting Started")
 }
-
-repositories {
-    maven {
-        url = uri("https://repo.gradle.org/gradle/libs")
-    }
-}
-
-dependencies {
-    constraints {
-        testImplementation("org.codehaus.groovy:groovy-all:2.5.3")
-    }
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-}
