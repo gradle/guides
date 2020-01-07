6fc8c60c90298f446efb825be58ecbaf8e0dbe4e
Daniel Lacasse <daniel@gradle.com>
2020-01-07 19:22:09 +0400

    Reverse the legacy and new plugin relationship

--- subprojects/gradle-guides-plugin/src/main/java/org/gradle/docs/samples/internal/LegacySamplesDocumentationPlugin.java
+++ subprojects/gradle-guides-plugin/src/main/java/org/gradle/docs/samples/internal/LegacySamplesDocumentationPlugin.java
@@ -2,14 +2,15 @@ package org.gradle.docs.samples.internal;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.docs.internal.BuildDocumentationPlugin;
 import org.gradle.docs.internal.DocumentationExtensionInternal;
 import org.gradle.docs.samples.Samples;
 
 @SuppressWarnings("UnstableApiUsage")
-public class SamplesPlugin implements Plugin<Project> {
+public class LegacySamplesDocumentationPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
-        project.getPluginManager().apply(SamplesDocumentationPlugin.class);
+        project.getPluginManager().apply(BuildDocumentationPlugin.class);
 
         // Register a samples extension to configure published samples
         SamplesInternal extension = project.getExtensions().getByType(DocumentationExtensionInternal.class).getSamples();
@@ -17,6 +18,5 @@ public class SamplesPlugin implements Plugin<Project> {
         project.getExtensions().add(Samples.class, "samples", extension);
         extension.getSamplesRoot().set(project.getLayout().getProjectDirectory().dir("src/samples"));
         extension.getTemplatesRoot().convention(project.getLayout().getProjectDirectory().dir("src/samples/templates"));
-
     }
 }
