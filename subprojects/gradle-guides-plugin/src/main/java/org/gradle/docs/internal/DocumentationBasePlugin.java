6b5989db6759d8d8e7210a2e39db1427473b53c4
Daniel Lacasse <daniel@gradle.com>
2020-01-07 15:23:08 +0400

    Add docsTest conventions

--- subprojects/gradle-guides-plugin/src/main/java/org/gradle/docs/internal/DocumentationBasePlugin.java
+++ subprojects/gradle-guides-plugin/src/main/java/org/gradle/docs/internal/DocumentationBasePlugin.java
@@ -2,6 +2,15 @@ package org.gradle.docs.internal;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.gradle.api.artifacts.dsl.DependencyHandler;
+import org.gradle.api.file.ProjectLayout;
+import org.gradle.api.tasks.SourceSet;
+import org.gradle.api.tasks.SourceSetContainer;
+import org.gradle.api.tasks.TaskContainer;
+import org.gradle.api.tasks.TaskProvider;
+import org.gradle.api.tasks.testing.Test;
+import org.gradle.language.base.plugins.LifecycleBasePlugin;
 
 public class DocumentationBasePlugin implements Plugin<Project> {
     public static final String DOCUMENTATION_GROUP_NAME = "Documentation";
@@ -9,7 +18,35 @@ public class DocumentationBasePlugin implements Plugin<Project> {
 
     @Override
     public void apply(Project project) {
-        project.getPluginManager().apply("lifecycle-base");
+        TaskContainer tasks = project.getTasks();
+        ProjectLayout layout = project.getLayout();
+
+        project.getPluginManager().apply("base");
         project.getExtensions().create(DOCUMENTATION_EXTENSION_NAME, DocumentationExtensionInternal.class);
+
+        TaskProvider<Task> check = tasks.named(LifecycleBasePlugin.CHECK_TASK_NAME);
+
+        // Testing
+        configureTesting(project, layout, tasks, check);
+    }
+
+    private void configureTesting(Project project, ProjectLayout layout, TaskContainer tasks, TaskProvider<Task> check) {
+        SourceSet sourceSet = project.getExtensions().getByType(SourceSetContainer.class).create("docsTest");
+
+        DependencyHandler dependencies = project.getDependencies();
+        dependencies.add(sourceSet.getImplementationConfigurationName(), dependencies.gradleTestKit());
+        dependencies.add(sourceSet.getImplementationConfigurationName(), "org.gradle:sample-check:0.9.0");
+        dependencies.add(sourceSet.getImplementationConfigurationName(), "org.slf4j:slf4j-simple:1.7.16");
+        dependencies.add(sourceSet.getImplementationConfigurationName(), "junit:junit:4.12");
+
+        TaskProvider<Test> docsTestTask = tasks.register(sourceSet.getName(), Test.class, task -> {
+            task.setGroup(LifecycleBasePlugin.VERIFICATION_GROUP);
+            task.setDescription("Test documentation");
+            task.setTestClassesDirs(sourceSet.getRuntimeClasspath());
+            task.setClasspath(sourceSet.getRuntimeClasspath());
+            task.setWorkingDir(layout.getProjectDirectory().getAsFile());
+        });
+
+        check.configure(task -> task.dependsOn(docsTestTask));
     }
 }
