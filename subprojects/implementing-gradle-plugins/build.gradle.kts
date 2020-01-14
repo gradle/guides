453ec84c5d1e511e8d1fcc3f40332952f055d86e
Daniel Lacasse <daniel@gradle.com>
2020-01-14 16:21:22 +0400

    Update exemplar to 0.12.5
    This version normalize incubating features

--- subprojects/implementing-gradle-plugins/build.gradle.kts
+++ subprojects/implementing-gradle-plugins/build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 }
 
 dependencies {
-    docsTestImplementation("org.gradle:sample-check:0.12.4")
+    docsTestImplementation("org.gradle:sample-check:0.12.5")
     docsTestImplementation(gradleTestKit())
 }
 
