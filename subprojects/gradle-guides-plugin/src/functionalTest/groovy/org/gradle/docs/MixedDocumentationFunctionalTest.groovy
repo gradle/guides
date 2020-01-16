f3959a74a5dd9c52ccaf1becbb924c05fd5440af
Daniel Lacasse <daniel@gradle.com>
2020-01-16 20:42:49 +0400

    Add mixed sample and guide documentation

---
+++ subprojects/gradle-guides-plugin/src/functionalTest/groovy/org/gradle/docs/MixedDocumentationFunctionalTest.groovy
@@ -0,0 +1,44 @@
+package org.gradle.docs
+
+import org.gradle.docs.guides.GuidesTrait
+import org.gradle.docs.samples.SamplesTrait
+import spock.lang.Ignore
+
+class MixedDocumentationFunctionalTest extends AbstractFunctionalTest implements SamplesTrait, GuidesTrait {
+    def "can assemble multiple documentation element type"() {
+        buildFile << applyDocumentationPlugin() << createGuide('demoGuide') << createSample('demoSample')
+        writeReadmeTo(file('src/docs/samples/demo-sample'))
+        writeKotlinDslSampleTo(file('src/docs/samples/demo-sample/kotlin'))
+        writeGroovyDslSampleTo(file('src/docs/samples/demo-sample/groovy'))
+        writeContentTo(file('src/docs/guides/demo-guide'))
+
+        when:
+        def result = build('assemble')
+
+        then:
+        result.assertTasksExecutedAndNotSkipped(':generateDemoSamplePage', ':generateWrapperForSamples', ':zipSampleDemoSampleGroovy', ':zipSampleDemoSampleKotlin', ':assembleDemoSampleSample', ':generateDemoGuidePage', ':assembleGuides', ':guidesMultiPage', ':generateSampleIndex', ':installSampleDemoSampleGroovy', ':generateSanityCheckTests', ':installSampleDemoSampleGroovyForTest', ':installSampleDemoSampleKotlin', ':installSampleDemoSampleKotlinForTest', ':assembleSamples', ':samplesMultiPage', ':assemble')
+    }
+
+    @Ignore
+    def "can create different documentation element type with the same name"() {
+        buildFile << applyDocumentationPlugin() << createGuide('demo') << createSample('demo')
+        writeReadmeTo(file('src/docs/samples/demo'))
+        writeKotlinDslSampleTo(file('src/docs/samples/demo/kotlin'))
+        writeGroovyDslSampleTo(file('src/docs/samples/demo/groovy'))
+        writeContentTo(file('src/docs/guides/demo'))
+
+        when:
+        def result = build('assemble')
+
+        then:
+        result.assertTasksExecutedAndNotSkipped(':assemble')
+    }
+
+    protected static String applyDocumentationPlugin() {
+        return """
+            plugins {
+                id 'org.gradle.documentation'
+            }
+        """
+    }
+}
