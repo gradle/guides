e3784a7c4131d02d1d7e92cce67406ca88a08a21
Donat Csikos <donat@gradle.com>
2020-05-04 21:44:48 +0200

    Allow dsl-specific Exemplar test declaration

--- subprojects/gradle-guides-plugin/src/main/java/org/gradle/docs/samples/internal/tasks/InstallSample.java
+++ subprojects/gradle-guides-plugin/src/main/java/org/gradle/docs/samples/internal/tasks/InstallSample.java
@@ -1,14 +1,21 @@
 package org.gradle.docs.samples.internal.tasks;
 
+import org.gradle.api.Action;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.file.ConfigurableFileCollection;
+import org.gradle.api.file.CopySpec;
 import org.gradle.api.file.DirectoryProperty;
 import org.gradle.api.file.FileTree;
-import org.gradle.api.tasks.InputFiles;
-import org.gradle.api.tasks.Internal;
-import org.gradle.api.tasks.OutputDirectory;
-import org.gradle.api.tasks.SkipWhenEmpty;
-import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.provider.Property;
+import org.gradle.api.specs.Spec;
+import org.gradle.api.specs.Specs;
+import org.gradle.api.tasks.*;
+import org.gradle.docs.samples.Dsl;
+
+import java.io.File;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Installs the sample's zip to the given directory.
@@ -26,11 +33,32 @@ public abstract class InstallSample extends DefaultTask {
     @OutputDirectory
     public abstract DirectoryProperty getInstallDirectory();
 
+    @Input
+    public abstract Property<Dsl> getDsl();
+
     @TaskAction
     private void doInstall() {
+        getSourceAsTree().getElements();
+
         getProject().sync(spec -> {
-            spec.from(getSource());
+            spec.from(getSource(), copySpec -> getFilteredExtensions().forEach(ext -> copySpec.exclude(ext) ));
             spec.into(getInstallDirectory());
         });
+    }
+
+    private Spec<? super File> getFilteredFiles() {
+        Dsl dsl = getDsl().getOrNull();
+        if (dsl == null) {
+            return Specs.satisfyAll();
+        }
+        EnumSet<Dsl> otherDsls = EnumSet.complementOf(EnumSet.of(dsl));
+        List<String> excluded = otherDsls.stream().map(d -> d.getDisplayName().toLowerCase() + ".sample.conf").collect(Collectors.toList());
+        return (Spec<File>) file -> !excluded.stream().anyMatch(ext -> file.getName().endsWith(ext));
+    }
+
+    private List<String> getFilteredExtensions() {
+        Dsl dsl = getDsl().get();
+        EnumSet<Dsl> otherDsls = EnumSet.complementOf(EnumSet.of(dsl));
+        return otherDsls.stream().map(d -> "**/*." +  d.getDisplayName().toLowerCase() + ".sample.conf").collect(Collectors.toList());
     }
 }
