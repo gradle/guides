b54ea3b26c8e306bf81565c0f369287ae2bf25e1
Donat Csikos <donat@gradle.com>
2020-05-05 09:20:17 +0200

    Clean up InstallSample implementation
    - clean up imports
    - remove unused method
    - remove leftover method call

--- subprojects/gradle-guides-plugin/src/main/java/org/gradle/docs/samples/internal/tasks/InstallSample.java
+++ subprojects/gradle-guides-plugin/src/main/java/org/gradle/docs/samples/internal/tasks/InstallSample.java
@@ -1,18 +1,18 @@
 package org.gradle.docs.samples.internal.tasks;
 
-import org.gradle.api.Action;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.file.ConfigurableFileCollection;
-import org.gradle.api.file.CopySpec;
 import org.gradle.api.file.DirectoryProperty;
 import org.gradle.api.file.FileTree;
 import org.gradle.api.provider.Property;
-import org.gradle.api.specs.Spec;
-import org.gradle.api.specs.Specs;
-import org.gradle.api.tasks.*;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.InputFiles;
+import org.gradle.api.tasks.Internal;
+import org.gradle.api.tasks.OutputDirectory;
+import org.gradle.api.tasks.SkipWhenEmpty;
+import org.gradle.api.tasks.TaskAction;
 import org.gradle.docs.samples.Dsl;
 
-import java.io.File;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -38,22 +38,10 @@ public abstract class InstallSample extends DefaultTask {
 
     @TaskAction
     private void doInstall() {
-        getSourceAsTree().getElements();
-
         getProject().sync(spec -> {
             spec.from(getSource(), copySpec -> getFilteredExtensions().forEach(ext -> copySpec.exclude(ext) ));
             spec.into(getInstallDirectory());
         });
-    }
-
-    private Spec<? super File> getFilteredFiles() {
-        Dsl dsl = getDsl().getOrNull();
-        if (dsl == null) {
-            return Specs.satisfyAll();
-        }
-        EnumSet<Dsl> otherDsls = EnumSet.complementOf(EnumSet.of(dsl));
-        List<String> excluded = otherDsls.stream().map(d -> d.getDisplayName().toLowerCase() + ".sample.conf").collect(Collectors.toList());
-        return (Spec<File>) file -> !excluded.stream().anyMatch(ext -> file.getName().endsWith(ext));
     }
 
     private List<String> getFilteredExtensions() {
