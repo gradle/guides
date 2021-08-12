08e6ebf84f81a7d4e9bc5783d07b79d1b9270de1
Benedikt Ritter <benedikt@gradle.com>
2021-08-12 12:31:10 +0200

    Drop build cache configuration script
    The build cache is configured by the enterprise conventions plugin.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,8 +3,6 @@ plugins {
     id("com.gradle.enterprise.gradle-enterprise-conventions-plugin").version("0.7.2")
 }
 
-apply(from = "gradle/build-cache-configuration.settings.gradle.kts")
-
 rootProject.name = "gradle-guides"
 
 includeBuild("subprojects/gradle-guides-plugin")
