6bd3d0f6ed822c022a3cd3de8821912ad3e43efe
Benedikt Ritter <benedikt@gradle.com>
2021-08-12 12:14:46 +0200

    Remove dead code
    None of the submodules applies the org.gradle.guide plugin, so this
    configuration is never executed.

--- build.gradle.kts
+++ build.gradle.kts
@@ -94,39 +94,3 @@ configure<org.ajoberstar.gradle.git.publish.GitPublishExtension> {
     }
 }
 
-// Test JVM codes
-allprojects {
-    pluginManager.withPlugin("org.gradle.guide") {
-        repositories {
-            maven {
-                url = uri("https://repo.gradle.org/gradle/libs-releases")
-            }
-        }
-
-        dependencies {
-            add("docsTestImplementation", gradleTestKit())
-            add("docsTestImplementation", "org.spockframework:spock-core:1.2-groovy-2.5")
-            add("docsTestImplementation", "commons-io:commons-io:2.5")
-            add("docsTestImplementation", "org.gradle.guides:test-fixtures:0.4")
-        }
-
-        // Passes the system property {@literal "samplesDir"} with the value {@literal "$projectDir/samples"} to {@code "test"} task.
-        tasks.named("docsTest", Test::class.java) {
-            val samplesBaseDir = project.file("samples")
-            inputs.files(samplesBaseDir).withPropertyName("samplesDir").withPathSensitivity(PathSensitivity.RELATIVE).optional()
-            jvmArgumentProviders += CommandLineArgumentProvider { listOf("-DsamplesDir=${samplesBaseDir.absolutePath}") }
-        }
-
-        // TODO: This is strictly for working around the tooling API bug regarding removing flackiness for build init tests
-        tasks.named("checkAsciidoctorGuideContents", AsciidoctorContentTest::class.java) {
-            gradleVersion.set("6.4")
-        }
-    }
-}
-
-// Attach checkGuides to check
-allprojects {
-    pluginManager.withPlugin("org.gradle.guide") {
-        tasks.named("check") { dependsOn("checkGuides") }
-    }
-}
