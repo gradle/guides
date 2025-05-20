17ecd0aed99b708e3f4e8714224d36418ecb0727
Christoph Obexer <cobexer@gradle.com>
2025-05-20 14:13:26 +0200

    Basic TeamCity & GitHub project setup
    Signed-off-by: Christoph Obexer <cobexer@gradle.com>

---
+++ .teamcity/src/main/kotlin/PublishPlugins.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import jetbrains.buildServer.configs.kotlin.ParameterDisplay
+import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
+
+object PublishPlugins : AbstractBuildType({
+    name = "Publish Guide Plugins"
+    type = Type.DEPLOYMENT
+
+    requirements {
+        contains("teamcity.agent.jvm.os.name", "Linux")
+    }
+
+    params {
+        param("env.JAVA_HOME", "%linux.java17.openjdk.64bit%")
+        password("env.GRADLE_PUBLISH_KEY", "%plugin.portal.publish.key%", display = ParameterDisplay.HIDDEN)
+        password("env.GRADLE_PUBLISH_SECRET", "%plugin.portal.publish.secret%", display = ParameterDisplay.HIDDEN)
+    }
+
+    steps {
+        gradle {
+            useGradleWrapper = true
+            gradleParams = "-Dgradle.publish.skip.namespace.check=true"
+            tasks = ":gradle-guides-plugin:publishPlugins"
+        }
+    }
+})
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-All content from this repository has been moved to the [Gradle User Manual](https://docs.gradle.org/current/).
+# Gradle Guides Plugin
 
-The Gradle User Manual sources are located here: https://github.com/gradle/gradle/tree/master/subprojects/docs
+All Gradle documentation content from this repository has been moved to the
+[Gradle User Manual](https://docs.gradle.org/current/).
