7223067e24d0afe78486afb5b1230f449aaae52a
Christoph Obexer <cobexer@gradle.com>
2026-02-17 12:42:43 +0100

    Build Infrastructure updates
    Signed-off-by: Christoph Obexer <cobexer@gradle.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.gradle.develocity").version("4.3.1")
-    id("com.autonomousapps.build-health").version("2.17.0")
-    id("io.github.gradle.gradle-enterprise-conventions-plugin").version("0.10.2")
+    id("com.gradle.develocity").version("4.3.2")
+    id("com.autonomousapps.build-health").version("3.5.1")
+    id("io.github.gradle.develocity-conventions-plugin").version("0.13.0")
 }
 
 rootProject.name = "gradle-guides"
