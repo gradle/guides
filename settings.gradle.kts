c737ea3a10c0fa11640f9a84faed8108b6427bd2
Christoph Obexer <cobexer@gradle.com>
2026-03-24 09:50:28 +0100

    Update Develocity Conventions Plugin
    Signed-off-by: Christoph Obexer <cobexer@gradle.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.gradle.develocity").version("4.3.2")
-    id("com.autonomousapps.build-health").version("3.5.1")
-    id("io.github.gradle.develocity-conventions-plugin").version("0.13.0")
+    id("com.autonomousapps.build-health").version("3.6.1")
+    id("io.github.gradle.develocity-conventions-plugin").version("0.14.1")
 }
 
 rootProject.name = "gradle-guides"
